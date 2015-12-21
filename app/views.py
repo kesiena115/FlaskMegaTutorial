4f7b0180d1fe3639d19479048dd8e358604514dc
Kesiena Owho-Ovuakporie <kesiena115@gmail.com>
2015-12-21 11:51:30 -0500

    Finished part 3

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +18,15 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	'''
+		The validate_on_submit method does all the form processing work. If you call it when the form is being 
+		presented to the user (i.e. before the user got a chance to enter data on it) then it will return 
+		False, so in that case you know that you have to render the template.
+	'''
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
