995a9a185639c6074a779900f91757a4dec7d0af
Kesiena Owho-Ovuakporie <kesiena115@gmail.com>
2015-12-23 11:24:06 -0500

    Part 6: Profile page and avatars

--- app/models.py
+++ app/models.py
@@ -1,13 +1,47 @@
 from app import db
+from hashlib import md5
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	nickname = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	about_me = db.Column(db.String(140))
+	last_seen = db.Column(db.DateTime)
+
+	'''
+		The is_authenticated property has a misleading name. In general this method should just return True unless the 
+		object represents a user that should not be allowed to authenticate for some reason.
+	'''
+	@property
+	def is_authenticated(self):
+		return True
+
+	# Should return True for users unless they are inactive, for example because they have been banned.
+	@property
+	def is_active(self):
+		return True
+
+	#  Should return True only for fake users that are not supposed to log in to the system.
+	@property
+	def is_anonymous(self):
+		return False
+
+	# Should return a unique identifier for the user, in unicode format.
+	def get_id(self):
+		try:
+			return unicode(self.id)  # python 2
+		except NameError:
+			return str(self.id)  # python 3
 
 	def __repr__(self):
 		return'<User %r>' % (self.nickname)
+
+	def avatar(self, size):
+		'''
+			Returns the URL of the user's avatar image, scaled to the requested size in pixels.
+		'''
+		return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,15 @@
-from flask import render_template, flash, redirect
-from app import app
-from .forms import LoginForm
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
+from datetime import datetime
+from .forms import LoginForm, EditForm
+from models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'nickname': 'Miguel'} # fake user
+    user = g.user
     posts = [  # fake array of posts
     	{ 
     		'author': {'nickname': 'John'}, 
@@ -18,15 +22,101 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
-	form = LoginForm()
-	'''
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    '''
 		The validate_on_submit method does all the form processing work. If you call it when the form is being 
 		presented to the user (i.e. before the user got a chance to enter data on it) then it will return 
 		False, so in that case you know that you have to render the template.
 	'''
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+	form = EditForm()
 	if form.validate_on_submit():
-		flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
-	return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
+		g.user.nickname = form.nickname.data
+		g.user.about_me = form.about_me.data
+		db.session.add(g.user)
+		db.session.commit()
+		flash('Your changes have been saved.')
+		return redirect(url_for('edit'))
+	else:
+		form.nickname.data = g.user.nickname
+		form.about_me.data = g.user.about_me
+		return render_template('edit.html', form=form)
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+'''
+	In the login view function we check g.user to determine if a user is already logged in. 
+	To implement this we will use the before_request event from Flask. Any functions that are decorated with 
+	before_request will run before the view  The current_user global is set by Flask-Login, so we just put a copy 
+	in the g object to have better access to it. With this, all requests will have access to the logged in user, 
+	even inside templates. each time a request is received. So this is the right place to setup our g.user variable. 
+	The current_user global is set by Flask-Login, so we just put a copy in the g object to have better access to it. 
+	With this, all requests will have access to the logged in user, even inside templates.
+'''
+@app.before_request
+def before_request():
+	g.user = current_user
+	if g.user.is_authenticated:
+		g.user.last_seen = datetime.utcnow()
+		db.session.add(g.user)
+		db.session.commit()
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+	user = User.query.filter_by(nickname=nickname).first()
+	if user == None:
+		flash('User %s not found.' % nickname)
+		return redirect(url_for('index'))
+	posts = [
+				{'author': user, 'body': 'Test post #1'},
+				{'author': user, 'body': 'Test post #2'}
+			]
+	return render_template('user.html', user=user, posts=posts)
