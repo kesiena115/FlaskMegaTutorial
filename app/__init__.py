34224abd8ec8e5bef37ab027a3dd477c9eff7df9
Kesiena Owho-Ovuakporie <kesiena115@gmail.com>
2015-12-23 13:42:48 -0500

    Part 7: Unit testing, error handlers, custom form validation and logging

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ import os
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
+from config import ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,6 +15,25 @@ lm.init_app(app)
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 # This helps Flask-Login to know the view that logs users in.
 lm.login_view = 'login'
+
+if not app.debug:
+	import logging
+	from logging.handlers import SMTPHandler
+	credentials = None
+	if MAIL_USERNAME or MAIL_PASSWORD:
+		credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+	mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+	mail_handler.setLevel(logging.ERROR)
+	app.logger.addHandler(mail_handler)
+
+	from logging.handlers import RotatingFileHandler
+	# We are limiting the size of a log file to one megabyte, and we will keep the last ten log files as backups.
+	file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	app.logger.setLevel(logging.INFO)
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+	app.logger.info('microblog startup')
 
 
 from app import views, models
